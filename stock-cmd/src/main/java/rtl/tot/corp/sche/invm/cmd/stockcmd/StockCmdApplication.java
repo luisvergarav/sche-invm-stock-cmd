ccd78e512dabc5ddac03cacc743b3b91a22ad53e
ext_lavergarav <ext_lavergarav@MOEXT_LAVER.Falabella.com>
2019-03-10 20:43:48 -0300

    initial commit

---
+++ stock-cmd/src/main/java/rtl/tot/corp/sche/invm/cmd/stockcmd/StockCmdApplication.java
@@ -0,0 +1,13 @@
+package rtl.tot.corp.sche.invm.cmd.stockcmd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockCmdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockCmdApplication.class, args);
+	}
+
+}
---
+++ stock-cmd/src/test/java/rtl/tot/corp/sche/invm/cmd/stockcmd/StockCmdApplicationTests.java
@@ -0,0 +1,16 @@
+package rtl.tot.corp.sche.invm.cmd.stockcmd;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StockCmdApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
